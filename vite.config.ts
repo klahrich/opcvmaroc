91c23b59ca99f20525178ac92cbb60af283014b4
Karim Lahrichi <karimlahrichi@hotmail.com>
2025-09-15 17:11:38 -0400

    vite updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src/frontend',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
